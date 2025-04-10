0d03bab4983642bcbbac80b1d9e7f3b6f1db5b3f
JV13 <cherkas.ivan13@gmail.com>
2025-04-10 18:04:28 +0300

    add few docker updates

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  githubToken: process.env.GITHUB_TOKEN,
+  vercelUrl: process.env.VERCEL_URL,
+  githubPagesUrl: process.env.GITHUB_PAGES_URL,
+  localUrl: process.env.LOCAL_URL,
+};
